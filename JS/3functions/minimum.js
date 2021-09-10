408f1e8699746c825bdf9f4e14526188669e4917
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-09-10 23:26:08 +0200

    functions

---
+++ JS/3functions/minimum.js
@@ -0,0 +1,13 @@
+function min (a, b) {
+    if (a < b) return a;
+    else return b;
+} 
+
+console.log (min(5, 10));
+
+const minTwo = (a, b) => {
+    if (a < b) return a;
+    else return b;
+}
+
+console.log (minTwo(4, 8));
