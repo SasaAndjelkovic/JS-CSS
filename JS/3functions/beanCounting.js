4f82544a8d5be9d3792def952e8c2d0301d9a6c5
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-09-22 21:51:12 +0200

    new commit

---
+++ JS/3functions/beanCounting.js
@@ -0,0 +1,17 @@
+const countChar = (string, ch) => {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == ch) {
+        counted += 1;
+      }
+    }
+    return counted;
+  }
+  
+  const countBs = string => countChar(string, "B");
+  
+  
+  console.log(countBs("BBC"));
+  // → 2
+  console.log(countChar("kakkerlak", "k"));
+  // → 4
