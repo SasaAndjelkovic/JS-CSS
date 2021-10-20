ca90711a54b2bf069f85396b06b026a1a2905a7c
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-10-20 09:05:04 +0200

    new commit

--- JS/challenges/easy.js
+++ JS/challenges/easy.js
@@ -30,3 +30,52 @@ const matchHouses = step => step === 0 ? 0 : 6 + (step - 1) * 5
 
 console.log(matchHouses(0));
 console.log(matchHouses(5));
+
+
+//4. Left Shift by Powers of Two
+const shiftToLeft = (x, y) => x * 2 ** y
+
+console.log(shiftToLeft(5, 2));
+console.log(shiftToLeft(10, 3));
+console.log(shiftToLeft(-32, 2));
+console.log(shiftToLeft(-6, 5));
+console.log(shiftToLeft(12, 4));
+console.log(shiftToLeft(46, 6));
+
+
+//5. Find the Smallest and Biggest Numbers
+const minMax = array => {
+	let min = +Infinity;
+	let max = -Infinity;
+	for (let i = 0; i < array.length; i++) {
+		if (min > array[i]) min = array[i];
+		if (max < array[i]) max = array[i];
+	}
+	return [min, max];
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
+console.log(minMax([2334454, 5]));
+console.log(minMax([1]));
+
+//another solution
+const minMax = array => [Math.min(...array), Math.max(...array)];
+console.log(minMax([1, 2, 3, 4, 5]));
+
+//another solution 2
+const minMax = array => {
+	array.sort((a, b) => a - b);
+	return [array[0], array[array.length-1]];
+  }
+console.log(minMax([1, 2, 3, 4, 5]));
+
+
+//6. Is it Time for Milk and Cookies?
+const timeForMilkAndCookies = dateToTest => (dateToTest.getMonth() === 0 && dateToTest.getDate() === 7);
+// dateToTest je u stvari (new Date(2013, 0, 7))
+
+console.log(timeForMilkAndCookies(new Date(2013, 0, 7))) // ➞ true
+console.log(timeForMilkAndCookies(new Date(2013, 11, 23))) // ➞ false
+console.log(timeForMilkAndCookies(new Date(3000, 11, 24))) // ➞ true
+
+console.log(new Date(2013, 0, 7));
