48790c6b473dfdbc8b22864da7132577393aea30
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-10-21 10:21:21 +0200

    new commit

--- JS/challenges/easy.js
+++ JS/challenges/easy.js
@@ -118,3 +118,13 @@ console.log(binary(10)); // ➞ "1010"
 //the best solution
 const binary = decimal => decimal.toString(2);
 
+
+//9. Check if One Array can be Nested in Another
+const canNest = (arr1, arr2) =>
+	Math.min(...arr1) > Math.min(...arr2) && 
+	Math.max(...arr1) < Math.max(...arr2);
+
+console.log(canNest([1, 2, 3, 4], [0, 6])) //➞ true
+console.log(canNest([3, 1], [4, 0])) // ➞ true
+console.log(canNest([9, 9, 8], [8, 9])) // ➞ false
+console.log(canNest([1, 2, 3, 4], [2, 3])) //➞ false
