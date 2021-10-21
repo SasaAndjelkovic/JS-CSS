97faf20ea4d70e04cf99648d1f773f12f009d6d7
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-10-21 12:34:50 +0200

    new commit

--- JS/challenges/easy.js
+++ JS/challenges/easy.js
@@ -128,3 +128,40 @@ console.log(canNest([1, 2, 3, 4], [0, 6])) //➞ true
 console.log(canNest([3, 1], [4, 0])) // ➞ true
 console.log(canNest([9, 9, 8], [8, 9])) // ➞ false
 console.log(canNest([1, 2, 3, 4], [2, 3])) //➞ false
+
+
+//10. RegEx Exercise 1: Find the Time
+const str = 'Breakfast at 09:00 in the room 123:456';
+const regexpFindTime =  /\b[0-9][0-9]:[0-9][0-9]\b/g; // bez g stampa ceo niz. Moze se dodati [0] na kraju u console.log
+
+console.log(str.match(regexpFindTime)); // -> 09:00
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Groups_and_Ranges
+
+
+
+//11. RegEx XIV: Group Ranges x|y
+const REGEXP =  /red flag|blue flag/g
+
+console.log("red flag blue flag".match(REGEXP)) // ➞ ["red flag", "blue flag"]
+console.log("yellow flag red flag blue flag green flag".match(REGEXP)) // ➞ ["red flag", "blue flag"]
+console.log("pink flag red flag black flag blue flag green flag red flag ".match(REGEXP)) // ➞ ["red flag", "blue flag", "red flag"]
+
+
+//12. Promises III: Native Promise, Introducing the Executor
+/*Promises are just objects that contain the outcome of asynchronous operations. 
+So when do you use one? When you want to control the outcome of an asynchronous operation. 
+All you have to do is wrap the asynchronous function with a promise constructor.
+
+The promise constructor requires you to pass a function called the executor which takes two parameters, resolve and reject. 
+Both are functions that you use to pass or reject a value that is usually the result of the async operation.
+setTimeout is a browser API that is very commonly used in tutorials to represent async operations. 
+After 1000ms has passed, we call the callback function in setTimeout() and pass a string "edabit" to the resolve function.
+
+Here's an example of a simple promise:*/
+
+let promise = new Promise ((resolve, reject) => {
+	setTimeout(() => {
+		resolve = 'edabit';
+	}, 1000)
+})
+//sve slozenije koncepte radi posle JavaScriptElokventno
