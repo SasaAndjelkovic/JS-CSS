cb6d2b08b691bfd6f3be69ad75d6017ff73ba5e7
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-10-22 19:42:55 +0200

    new commit

--- JS/challenges/easy.js
+++ JS/challenges/easy.js
@@ -221,5 +221,24 @@ const detectWord = str =>
 const detectWord = str => str.replace(/[A-Z]/g, '');
 
 
-//16. 
+//16. Older Than Me
 
+class Person {
+	constructor (name, age) {
+	this.name = name;
+	this.age = age;
+	}
+	compareAge = a => 
+	`${a.name} is ${a.age > this.age ? 'older than' : 
+		a.age < this.age ? 'younger than' : 'the same age as'} me.`;
+}	
+
+
+
+const p1 = new Person("Samuel", 24);
+const p2 = new Person("Joel", 36);
+const p3 = new Person("Lily", 24);
+
+console.log(p1.compareAge(p2)) // -> "Joel is older than me."
+console.log(p2.compareAge(p1)) // ➞ "Samuel is younger than me."
+console.log(p1.compareAge(p3)) // ➞ "Lily is the same age as me."
