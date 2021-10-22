76d2e46e81ce1031bd1690b7b6c7555991ecf224
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-10-22 22:20:01 +0200

    new commit

--- JS/challenges/easy.js
+++ JS/challenges/easy.js
@@ -252,3 +252,12 @@ const sortDrinkByPrice = drnc =>
 
 
 console.log(sortDrinkByPrice(drinks)) // ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
+
+
+//18. Tuck in Array   (Use the spread syntax to solve this challenge.)
+const tuckIn = (arr1, arr2) => [arr1[0], ...arr2, arr1[arr1.length-1]]
+
+	
+console.log(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9])) // ➞ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(tuckIn([15,150], [45, 75, 35])) // ➞ [15, 45, 75, 35, 150]
+console.log(tuckIn([[1, 2], [5, 6]], [[3, 4]])) // ➞ [[1, 2], [3, 4], [5, 6]]
