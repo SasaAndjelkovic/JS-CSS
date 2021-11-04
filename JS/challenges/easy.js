a1d68ba1d7b899256d40e89eb9ffd2da0da21f25
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-11-04 16:41:27 +0100

    new commit

--- JS/challenges/easy.js
+++ JS/challenges/easy.js
@@ -415,3 +415,13 @@ console.log(isTrue("8<7")) // ➞ false
 console.log(isTrue("5=13")) // ➞ false
 console.log(isTrue("15>4")) // ➞ true
 
+
+//30.Sort by String Length
+const sortByLength = obj =>
+	obj.sort((a, b) => a.length - b.length);
+
+console.log(sortByLength(["a", "ccc", "dddd", "bb"])) // ➞ ["a", "bb", "ccc", "dddd"]
+console.log(sortByLength(["apple", "pie", "shortcake"])) // ➞ ["pie", "apple", "shortcake"]
+console.log(sortByLength(["may", "april", "september", "august"])) // ➞ ["may", "april", "august", "september"]
+console.log(sortByLength([])) // ➞ []
+
