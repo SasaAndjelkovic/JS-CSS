159108f702993b4a511e4d068aa8b7e663c11dfb
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-12-17 09:19:31 +0100

    new commit

--- JS/JavaScript Algorithms/basicJavaScript.js
+++ JS/JavaScript Algorithms/basicJavaScript.js
@@ -27,3 +27,60 @@ b = a;
 //5. Initializing Variables with the Assignment Operator
 var a = 9;
 
+
+//6. Declare String Variables
+var myFirstName = 'Sasa';
+var myLastName = 'Andjelkovic';
+
+
+//7. Understanding Uninitialized Variables
+var a;
+var b;
+var c;
+var d;
+var e;
+
+d = a + b; // -> NaN
+
+c = 'I am an ';
+
+e = c + a; // -> I am an undefined
+
+a = 5;
+b = 10;
+
+a = a + 1;
+b = b + 5;
+c = c + 'String!';
+
+console.log (d, a, b, c, e);
+
+
+//8. Understanding Case Sensitivity in Variables
+
+//Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+//Variable assignments
+studlyCapVar = 10;
+properCamelCase = 'A String';
+titleCaseOver = 9000;
+
+
+//9. Explore Differences Between the var and let Keywords
+var camper = 'James';
+var camper = 'David';
+console.log(camper) // -> David
+
+let camper = 'James';
+let camper = 'David';
+console.log(camper) // ->SyntaxError: Identifier 'camper' has already been declared
+
+
+//10. Declare a Read-Only Variable with the const Keyword
+const FCC = 'freeCodeCamp';
+let fact = 'is cool!';
+fact = 'is awesome!'; //obrati paznju: fact je ipak promenjen jer je u drugoj liniji bez let
+console.log(FCC, fact); //freeCodeCamp is awesome!
