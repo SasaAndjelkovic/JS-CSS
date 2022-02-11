61fa302809fee9918785735bec2849506faeb792
unknown <sandjelkovickg@gmail.com>
2022-02-11 21:47:53 +0100

    new commit

--- JS/JavaScript Algorithms/03regularExpressions.js
+++ JS/JavaScript Algorithms/03regularExpressions.js
@@ -57,10 +57,138 @@ let codingRegex = /coding/;
 let result = extractStr.match(codingRegex);
 
 
+
 //6. Find More Than the First Match
-let testStr = 'Repeat, repeat, repeat';
+let testStr = 'Repeat, Repeat, Repeat';
 let ourRegex = /Repeat/;
-console.log(testStr.match(ourRegex));
+testStr.match(ourRegex); // -> ['Repeat']
+
+let repeatRegex = /Repeat/g;
+testStr.match(repeatRegex);  // -> ['Repeat, Repeat, Repeat']
+
+
+let twinkleStar = 'Twinkle, twinkle, little star';
+let starRegex = /twinkle/gi
+let result = twinkleStar.match(starRegex);
+
+
+
+//7. Match Anything with WildCard Period
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./
+huRegex.test(humStr); // -> true
+huRegex.test(hugStr); // -> true
+
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let result = unRegex.test(exampleStr);
+
+
+
+//8. Match Single Character with Multiple Possibilities
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aio]g/;
+bigStr.match(bgRegex);  // -> ["big"]
+bagStr.match(bgRegex);  // -> ["bag"]
+bugStr.match(bgRegex);  // -> null
+console.log(bogStr.match(bgRegex)[0]);  // -> ["bog"]
+
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi
+let result = quoteSample.match(vowelRegex);
+console.log(result);   /*
+                        [
+                             'e', 'a', 'e', 'o', 'u', 'i',
+                             'e', 'a', 'o', 'e', 'o', 'e',
+                             'I', 'a', 'e', 'o', 'o', 'e',
+                             'i', 'o', 'e', 'o', 'i', 'e',
+                             'i'
+                        ]
+                        */
+
+
+
+//9. Match Letters of the Alphabet
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/     //at je poslednji deo reci
+catStr.match(bgRegex);
+batStr.match(bgRegex);
+matStr.match(bgRegex);
+
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi;
+let result = quoteSample.match(alphabetRegex);
+console.log(result);   /*
+                        [
+  'T', 'h', 'e', 'q', 'u', 'i', 'c',
+  'k', 'b', 'r', 'o', 'w', 'n', 'f',
+  'o', 'x', 'j', 'u', 'm', 'p', 's',
+  'o', 'v', 'e', 'r', 't', 'h', 'e',
+  'l', 'a', 'z', 'y', 'd', 'o', 'g'
+]*/
+
+
+
+//10. Match Numbers and Letters of the Alphabet
+let jennyStr = "Jenny8675309";
+let myRegex = /[a - z0 - 9]/ig;
+jennyStr.match(myRegex);
+
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi;
+let result = quoteSample.match(myRegex);
+
+
+
+//11. Match Single Characters Not Specified
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/gi;
+let result = quoteSample.match(myRegex);
+console.log(result);
+/*
+[
+  ' ', 'b', 'l',
+  'n', 'd', ' ',
+  'm', 'c', '.'
+]
+*/
+
+
+
+//12. Match Characters that Occur One or More Times
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g;
+let result = difficultSpelling.match(myRegex);
+console.log(result);    // -> [ 'ss', 'ss' ]
+
+
+
+//13. Match Charachters that Occur Zero or More Times
+let soccerWord = "goooooool!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex);   // -> ["goooooo"]
+gPhrase.match(goRegex);  // -> ["g"]
+oPhrase.match(goRegex);  // -> null
+
+
+let chewieQuote = "Aaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/;
+let result = chewieQuote.match(chewieRegex);
+
+
+
 
 
 
